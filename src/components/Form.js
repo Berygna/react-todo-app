f332251f39027da3f0eb0d717edd2662b16f6171
Berygna <berygna@gmail.com>
2024-03-02 23:36:08 +0900

    Add List Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,7 @@ export default function Form({handleSubmit, value, setValue}) {
   };
 
   return (
-    <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex pt-2">
       <input
         type="text"
         name="value"
@@ -14,12 +14,12 @@ export default function Form({handleSubmit, value, setValue}) {
         placeholder="해야할 일을 입력하세요."
         value={value}
         onChange={handleChange}
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
       />
       <input
         type="submit"
         value="입력"
-        className="btn"
-        style={{ flex: '1' }}
+        className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200" 
       />
   </form>
 
